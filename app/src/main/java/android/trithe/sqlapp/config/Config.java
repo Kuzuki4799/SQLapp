4f5277b40f13a5868d7c65853567ec0e30850eb8
Kuzuki <nguyentrithe040799@gmail.com>
2019-10-29 19:32:40 +0700

    New

--- app/src/main/java/android/trithe/sqlapp/config/Config.java
+++ app/src/main/java/android/trithe/sqlapp/config/Config.java
@@ -3,7 +3,7 @@ package android.trithe.sqlapp.config;
 public class Config {
     public static final int TIME_OUT = 30000;
     private static final String IP_CONFIG = "192.168.0.116";
-    public static final String API_URL = "http://" + IP_CONFIG + "/RetrofitvsmSql/";
+    public static final String API_URL = "http://" + IP_CONFIG + "/RetrofitvsmSqls/";
 
     public static final String URL_FB_PROFILE = "fb://profile/";
     public static final String URL_FB_INFO = "/info";
